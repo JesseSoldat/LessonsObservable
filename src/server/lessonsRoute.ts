7260d0cfb2ebd3aba894d1accea0d1245971e7af
JesseSoldat <jsoldat@hotmail.com>
2017-09-29 15:43:16 -0400

    error component works

--- src/server/lessonsRoute.ts
+++ src/server/lessonsRoute.ts
@@ -4,10 +4,13 @@ import * as _ from 'lodash';
 export function lessonsRoute(req, res) {
   console.log(req.query);
   const random = Math.ceil(Math.random() * 10);
-  // if(random % 2 === 1) {
-  //   res.sendStatus(500);
-  //   return;
-  // }
+  console.log('random', random);
+  if (random % 2 == 1) {
+    console.log('error 500');
+    
+    res.sendStatus(500);
+    return;
+  }
 
   const courseId = parseInt(req.query['courseId']) -1,
         pageNumber = parseInt(req.query['pageNumber']),
--- src/server/server.ts
+++ src/server/server.ts
@@ -19,5 +19,5 @@ app.route('/api/courses/:id').get(courseRoute);
 app.route('/api/lessons').get(lessonsRoute);
 app.route('/api/lessons/:id').get(lessonDetailRoute);
 
-app.listen(8090, () => {
+app.listen(8091, () => {
 });
